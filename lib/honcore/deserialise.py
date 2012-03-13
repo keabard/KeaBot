957e66cd246fdf35c362c20f9c9deef2e40388b8
keabard <gouki46@hotmail.com>
2012-03-13 16:39:47 +0100

    Repository initializatoin

---
+++ lib/honcore/deserialise.py
@@ -0,0 +1,143 @@
+""" 
+HoNCore. Python library providing connectivity and functionality
+with HoN's chat server.
+"""
+
+from common import User, Channel, Account
+from exceptions import *
+
+from lib import phpserialize as phpd
+
+""" Deserialises the php stuff and creates objects, not really... big... since there's this like, library... already..
+
+    There seems to be a difference between super_id and account_id, atleast in a trial account. 
+    A trial account that I created would not be able to use this program as the super_id was
+    different to the account id. Replacing super_id with account_id is the fix, but as to why
+    is a case to be solved.
+
+    If a user does not have any buddies then the buddy array is structured as follows.
+
+    buddy {
+        error {
+            "No buddies found" 
+        }
+    }
+
+    If a user has a buddy then it is structured as the following.
+
+    buddy_list {
+        <account_id> {
+            <buddy_data_array>
+        }
+    }
+
+"""
+
+def parse_raw(raw):
+    """ Takes raw serialised data and returns raw non-serialised data """
+    return phpd.loads(raw)
+
+def parse(raw):
+    """ Takes raw login data and a user account, extracts useful information
+        from the data and populates the account.
+    """
+
+    try:
+        data = phpd.loads(raw)
+    except ValueError, e:
+        if e == 'unexpected opcode':
+            raise MasterServerError(108)
+    
+    # If an auth key is found then there has been an authentication error.
+    if 'auth' in data:
+        raise MasterServerError(102) # Incorrect username/password
+    
+    """ When the servers are down for maintenance the following array is returned
+        by the master server:
+            a:2{i:0,b:1,s:16:"vested_threshold",i:5;}
+        See if that is true for all maintenance periods and maybe use that to consider
+        when maintenance is taking place.
+    """
+
+    try:
+        account = get_basic_info(data)
+    except KeyError:
+        raise MasterServerError(101) # Could not obtain login data
+
+    # List of new user objects for the client to learn about.
+    new_users = []
+
+
+    # Note: Try/Excepts are executed quicker than if 'xx' in data statements.
+
+    try:
+        account.buddy_list, users = get_buddies(data['buddy_list'][account.account_id])
+        for user in users:
+            new_users.append(user)
+    except KeyError:
+        pass
+
+    try:
+        account.clan_member_list, users = get_clan_memebrs(data['clan_member_info'])
+        for user in users:
+            new_users.append(user)
+    except KeyError:
+        pass
+
+    try:
+        account.ban_list = get_ban_list(data['banned_list'])
+    except KeyError:
+        pass
+
+    try:
+        account.ignore_list = get_ignore_list(data['ignored_list'])
+    except KeyError:
+        pass
+    
+    return (account, new_users)
+
+def get_basic_info(data):
+    try:
+        super_id = int(data['super_id'])
+        account_id = int(data['account_id'])
+        try:
+            clan_tag = "[%s]" % data['clan_member_info']['tag']
+        except KeyError:
+            clan_tag = ''
+        nickname = "%s%s" % (clan_tag, data['nickname'])
+        cookie = data['cookie']
+        auth_hash = data['auth_hash']
+        chat_url = data['chat_url']
+        ip = data['ip']
+        account = Account(super_id, account_id, nickname, cookie, auth_hash, chat_url, ip)
+    except KeyError, e:
+        raise
+    return account
+    
+def get_buddies(buddylist):
+    """ NOTE: It is not possible to get flag or status here. """
+    buddy_list = []
+    new_users = []
+    for userKey in buddylist:
+        accid = int(buddylist[userKey]['buddy_id'])
+        nick = buddylist[userKey]['nickname']
+        clan_tag = buddylist[userKey]['clan_tag']
+        if clan_tag:
+            nick = "[%s]%s" % (clan_tag, nick)
+        buddy_list.append(accid)
+        new_users.append(User(accid, nick))
+    return (buddy_list, new_users)
+
+def get_ban_list(data):
+    ban_list = {}
+    return ban_list
+
+def get_ignore_list(data):
+    ignore_list = {}
+    return ignore_list
+
+def get_clan_memebrs(data):
+    clan_members = []
+    new_users = []
+    return (clan_members, new_users)
+
---
+++ lib/honcore/requester.py
@@ -0,0 +1,118 @@
+""" 
+HoNCore. Python library providing connectivity and functionality
+with HoN's chat server.
+"""
+
+import hashlib, urllib2
+from exceptions import *
+from httplib import BadStatusLine
+
+
+""" Sends requests to the HoN master servers.
+    These are just basic HTTP get requests which return serialised php.
+
+    A version of '2.1.0' forced the connection to be dropped by the server, changing it back to a 4 'digit' version fixes. Guess the version string
+    must be 4 digits..
+
+    TODO:
+         * If sending a logout request times out then it's a bit... confusing as to what's going on. Could need cleaning up.
+"""
+_config_defaults = {
+    "masterserver" : "http://masterserver.hon.s2games.com/", 
+    "basicserver" : "http://heroesofnewerth.com/", 
+    "version": "2.5.7.0"
+}
+
+class Requester:
+    def __init__(self):
+        self.config = _config_defaults
+
+    def httpget(self, base, url):
+        url = base + url
+        header = { 'User-Agent' : "S2 Games/Heroes of Newerth/%s/lac/x86-biarch" % self.config["version"]}
+        req = urllib2.Request(url, None, header)
+        try:
+            response = urllib2.urlopen(req, timeout=20)
+            return response.read()
+        except urllib2.HTTPError, e:
+            # TODO : Find out what errors to catch.
+            print e.code
+            print e.read()
+            raise MasterServerError(107)
+        except urllib2.URLError, e:
+            code = e.reason[0]
+            if code == 104: # Connection reset by peer
+                raise MasterServerError(110)
+            elif code == 111:
+                raise MasterServerError(111)
+            elif code == "timed out":
+                raise MasterServerError(112)
+            elif code == -5:
+                raise MasterServerError(114)
+            else:
+                print e
+                print code
+                raise MasterServerError(107)
+        except BadStatusLine, e:
+            raise MasterServerError(109, e)
+
+    def httpost(self, url):
+        """ WHY """
+        pass
+
+    def login(self, username, password):
+        """ Requests basic information about the user's account """
+        url = "client_requester.php?f=auth&login=%s&password=%s" % (username, password)
+        return self.httpget(self.config['masterserver'], url)
+
+    def logout(self, cookie):
+        """ Sends a logout 'request'. 
+            Returns a:2:{i:0;b:1;s:12:"client_disco";s:2:"OK";} on a successful logout.
+        """
+        url = "client_requester.php?f=logout&cookie=%s" % cookie 
+        return self.httpget(self.config['masterserver'], url)
+
+    def motd(self):
+        """ Requests the message of the day list from the server.
+            Contains the last 6 message of the day(s??) Messages of the day?.
+        """
+        url = "/gen/client_motd3.php?data=retrieve"
+        return self.httpget(self.config['basicserver'], url) 
+        
+    def server_list(self, cookie, gametype):
+        """ Requests a server list from the server.
+            Contains the entire list of servers.
+        """
+        url = "client_requester.php?f=server_list&cookie=%s&gametype=%s"%(cookie, gametype)
+        return self.httpget(self.config['masterserver'], url)
+
+    def nick2id(self, nickname):
+        pass
+
+    def new_buddy(self, cookie, aid, bid):
+        pass
+
+    def remove_buddy(self, cookie, aid, bid):
+        pass
+
+    def new_banned(self, cookie, aid, bid, reason):
+        pass
+
+    def remove_banned(self, cookie, aid, bid, reason):
+        pass
+
+    def new_ignored(self, cookie, aid, iid, reason):
+        pass
+
+    def remove_ignored(self, cookie, aid, iid, reason):
+        pass
+
+    def stats_request(self, aid):
+        pass
+
+    def stats_request_ranked(self, aid):
+        pass
+
+    def patcher(self, version, os, arch):
+        pass
+
